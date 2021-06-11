678a1422478834797db67b32fbd5d154854aebfc
KarinKwint <83405911+KarinKwint@users.noreply.github.com>
2021-06-11 14:32:28 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let name = 'Karin';
+console.log("Karin");
+
+let Multiplication
+let y = 5;
+let x = y * 2;
+console.log(x);
+
+let Substraction
+let a = 5
+let c = a - 2
+console.log(c);
+
+let age = 'negenenveertig';
+console.log(age);
+
